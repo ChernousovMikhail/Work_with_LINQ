42800e530a27b649fdb4c9ec95a654c387e861cb
ChernousovMikhail <67697154+ChernousovMikhail@users.noreply.github.com>
2022-01-16 21:33:02 +0300

    12

---
+++ Work with LINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Work_with_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+            myDictionary.Add("Александр", 25);
+            myDictionary.Add("Петр", 35);
+            myDictionary.Add("Иван", 20);
+            myDictionary.Add("Семен", 15);
+            myDictionary.Add("Андрей", 5);
+            var result =
+                from string name in myDictionary.Keys
+                where myDictionary[name] > 18
+                orderby name
+                select name;
+
+            Console.WriteLine("Старше 18:");
+
+            foreach (string name in result)
+                Console.WriteLine(name);
+        }
+    }
+}
